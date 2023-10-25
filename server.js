724ee33a0a74f06b173337aff4fc69c703856f6c
GainsGoblin <83374187+GainsGoblin@users.noreply.github.com>
2023-10-25 18:32:51 +0200

    Correct hash

--- server.js
+++ server.js
@@ -623,18 +623,16 @@ class App {
     }
 
     async validateHash(req, chainId) {
-        const TYPEHASH = ethers.keccak256(ethers.toUtf8Bytes("ForwardRequest(address from,address to,bytes32 salt,uint256 deadline,bytes data)"));
-
         const from = req.from;
         const to = req.to;
         const salt = req.salt;
         const deadline = req.deadline;
         const data = req.data;
 
-        const hash = ethers.solidityPackedKeccak256(
-            ['bytes32', 'uint256', 'uint256', 'bytes32', 'uint256', 'bytes32'],
-            [TYPEHASH, from, to, salt, deadline, ethers.keccak256(data)]
-        );
+        const hash = ethers.keccak256(ethers.AbiCoder.defaultAbiCoder().encode(
+            ["address", "address", "bytes32", "uint256", "bytes"],
+            [from, to, salt, deadline, data]
+        ));
 
         console.log("Hash: " + hash);
 
